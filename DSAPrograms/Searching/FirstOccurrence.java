452eb7ea0e674e093f9a6e09274c6c1695d3f119
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-09-09 14:56:02 +0530

    updated

--- DSAPrograms/Searching/FirstOccurrence.java
+++ DSAPrograms/Searching/FirstOccurrence.java
@@ -3,6 +3,28 @@ package DSAPrograms.Searching;
 public class FirstOccurrence {
 	public static void main(String[] args) {
 		int[] arr = {1,2,2,2,2,2,2,2,2,2,3,4};
+		int target=2;
+		
+		int left = 0;
+		int right = arr.length-1;
+		
+		int ans = -1;
+		
+		while(left<=right) {			
+			int mid = (left+right)/2;
+			
+			if(arr[mid]==target) {
+				ans = mid;
+				right=mid-1; //go to the left to find first occur
+				
+			} else if(target<arr[mid]) {
+				right=mid-1;
+			} else {
+				left = mid+1;
+			}
+			
+		}
+		System.out.println("First occurance is: "+ans);
 	}
 
 }
