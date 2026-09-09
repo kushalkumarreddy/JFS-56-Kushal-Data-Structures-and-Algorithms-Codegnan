7a7f2fe17b6be69c40021a0e3b31b63ef973310d
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-09-09 16:00:30 +0530

    updated

--- DSAPrograms/Searching/FindInsertPosition.java
+++ DSAPrograms/Searching/FindInsertPosition.java
@@ -6,8 +6,16 @@ public class FindInsertPosition {
 		int arr[] = {1,3,4,5};
 		int target = 2;
 		
+		int left = 0;
+		int right = arr.length-1;
 		
-		
+		int mid = (left+right)/2;
+		if(arr[mid]<target) {
+			left=mid+1;
+		} else {
+			right = mid-1;
+		}
+		System.out.println("position is: "+left);
 	}
 
 }
